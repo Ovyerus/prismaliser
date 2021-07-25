a4dbb99cf9350b271325c6fa120fceea19fe07c0
Michael Mitchell <iamovyerus@gmail.com>
2021-07-25 22:29:14 +1000

    fix: try add prisma sdk as an external

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@prisma/sdk");
+    }
+
+    return config;
+  },
 };
