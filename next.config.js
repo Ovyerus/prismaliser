410214355d2919846563724e05186a117ff2f141
Michael Mitchell <iamovyerus@gmail.com>
2021-07-26 00:44:52 +1000

    fix: try disabling webpack5

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("@prisma/sdk");
-    }
-
-    return config;
+  future: {
+    webpack5: false,
   },
 };
