9fd7e373427a947ba96031046d8989a6b0203e2f
Michael Mitchell <iamovyerus@gmail.com>
2021-10-31 21:49:31 +1100

    fix: update prisma inclusion path

--- util/server.ts
+++ util/server.ts
@@ -1,13 +1,14 @@
 import path from "path";
 
 // https://github.com/Ovyerus/prisma-sdk-vercel-repro/pull/1/commits/a01508e8980426b18be690d386c9ffddaaa472e6
+// TODO: see if we can have an automated check in the Vercel build which ensures the presence of these
 export const fixPrisma = () => {
   path.join(
     process.cwd(),
-    "node_modules/@prisma/engines/query-engine-rhel-openssl-1.0.x"
+    "node_modules/@prisma/engines/libquery_engine-rhel-openssl-1.0.x"
   );
-  path.join(
-    process.cwd(),
-    "node_modules/@prisma/engines/query-engine-windows.exe"
-  );
+  // path.join(
+  //   process.cwd(),
+  //   "node_modules/@prisma/engines/query-engine-windows.exe"
+  // );
 };
