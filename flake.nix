1164c6bfae80d37d9b3ee5f93fee5c270784a4a6
Ovyerus <ovy@ovyerus.com>
2024-07-26 19:33:46 +1000

    chore(flake): ditch flake-utils

--- flake.nix
+++ flake.nix
@@ -1,27 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              nodejs_20
-              yarn
-            ];
-          };
-      }
-    );
+  outputs = {nixpkgs, ...}: let
+    forSystems = fn:
+      nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: fn nixpkgs.legacyPackages.${system});
+    defaultForSystems = fn: forSystems (pkgs: {default = fn pkgs;});
+  in {
+    devShells = defaultForSystems (pkgs: with pkgs; mkShell {buildInputs = [nodejs_20 yarn];});
+  };
 }
